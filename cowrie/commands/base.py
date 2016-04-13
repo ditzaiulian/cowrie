0cc8082c2248da1ef570a126006e2602db03f68c
Cristi Dita <ditza_iulian@yahoo.com>
2016-04-13 14:37:24 +0300

    bug

--- cowrie/commands/base.py
+++ cowrie/commands/base.py
@@ -247,7 +247,7 @@ class command_ps(HoneyPotCommand):
             ('root      ', '2133', '   1', '  0.0', '  0.1', '   2180', '   620', ' ?        ', 'S<s  ', 'Nov06', '   0:00 ', 'dhclient3 -pf /var/run/dhclient.eth0.pid -lf /var/lib/dhcp3/dhclien',),
             ('root      ', '4969', '   1', '  0.0', '  0.1', '   5416', '  1024', ' ?        ', 'Ss   ', 'Nov08', '   0:00 ', '/usr/sbin/sshd: %s@pts/0' % user,),
             ('%s'.ljust(8) % user, '5673', '2103', '  0.0', '  0.2', '   2924', '  1540', ' pts/0    ', 'Ss   ', '04:30', '   0:00 ', '-bash',),
-            ('%s'.ljust(8) % user, '5679', '2103', '  0.0', '  0.1', '   2432', '   928', ' pts/0    ', 'R+   ', str(time.strftime('%H:%M:%S'), utils.uptime(self.protocol.uptime())), '   0:00 ', 'ps %s' % ' '.join(self.args),)
+            ('%s'.ljust(8) % user, '5679', '2103', '  0.0', '  0.1', '   2432', '   928', ' pts/0    ', 'R+   ', str(self.protocol.uptime()), '   0:00 ', 'ps %s' % ' '.join(self.args),)
             )
         for i in range(len(output)):
             if i != 0:
